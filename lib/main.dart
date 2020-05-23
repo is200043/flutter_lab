936c491a89bba711fce84d57171c148b14d4ae95
Chonlakorn Punphopthaworn <chonlakorn.pun@gmail.com>
2020-05-23 12:48:37 +0700

    show correct

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,15 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _answerText = "";
+  var _isCorrect = false;
 
   void _onPressTomHank() {
     print('This is Tom Hank');
     setState(() {
       _questionIndex = _questionIndex + 1;
+      _answerText = "True";
+      _isCorrect = true;
     });
     print('Index = $_questionIndex');
   }
@@ -33,23 +37,68 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(children: <Widget>[
           Question(questions[_questionIndex]),
-          RaisedButton(
-            child: Text('Justin Bieber'),
-            onPressed: () => print('I am clicking Justin Bieber'),
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            child: RaisedButton(
+              child: Text('Justin Bieber'),
+              onPressed: () {
+                print('I am clicking Justin Bieber');
+                setState(() {
+                  _answerText = "False";
+                  _isCorrect = false;
+                });
+              },
+            ),
           ),
-          RaisedButton(
-            child: Text('Bruce Willis'),
-            onPressed: () {
-              print('Bruce Willis is my dad.');
-            },
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            child: RaisedButton(
+              child: Text('Bruce Willis'),
+              onPressed: () {
+                print('Bruce Willis is my dad.');
+                setState(() {
+                  _answerText = "False";
+                  _isCorrect = false;
+                });
+              },
+            ),
           ),
-          RaisedButton(
-            child: Text('Tom Hank'),
-            onPressed: _onPressTomHank,
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            child: RaisedButton(
+              child: Text('Tom Hank'),
+              onPressed: _onPressTomHank,
+            ),
           ),
-          RaisedButton(
-            child: Text('Nicolas Cage'),
-            onPressed: () => print('Click Nicolas Cage'),
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            child: RaisedButton(
+              child: Text('Nicolas Cage'),
+              onPressed: () {
+                print('Click Nicolas Cage');
+                setState(() {
+                  _answerText = "False";
+                  _isCorrect = false;
+                });
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            child: _isCorrect
+                ? Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: Text(
+                      _answerText,
+                      style: TextStyle(
+                        fontSize: 40,
+                        foreground: Paint()
+                          ..style = PaintingStyle.stroke
+                          ..strokeWidth = 6
+                          ..color = Colors.blue[700],
+                      ),
+                    ))
+                : Text(_answerText),
           ),
         ]),
       ),
